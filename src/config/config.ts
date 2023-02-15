21d4899ce2615e8e0aaeb8c6632cf7c64cc88c91
ManuelMarmol2710 <92897105+ManuelMarmol2710@users.noreply.github.com>
2023-02-15 19:39:55 -0400

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,7 @@ export default {
   DB: {
     URI:
       process.env.MONGODB_URI ||
-      "mongodb+srv://movilesproyecto:<12345>@cluster0.htp15ow.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://movilesproyecto:12345@cluster0.htp15ow.mongodb.net/test",
  
     USER: process.env.MONGODB_USER || "",
 
