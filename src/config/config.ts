2b217a50cfa8f6116b142d4edf4f027eab917aa2
ManuelMarmol2710 <92897105+ManuelMarmol2710@users.noreply.github.com>
2023-02-15 23:18:22 -0400

    Add files via upload

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,21 +1,21 @@
 export default {
-  jwtSecret: process.env.JWT_SECRET || "somesecrettoken",
-
-  DB: {
-    URI:
-      process.env.MONGODB_URI ||
-      "mongodb+srv://movilesproyecto:12345@cluster0.htp15ow.mongodb.net/test",
- 
-    USER: process.env.MONGODB_USER || "",
-
-    PASSWORD: process.env.MONGODB_PASSWORD || "",
-
-    NAME: process.env.MONGODB_NAME || "",
-
-    LAST_NAME: process.env.MONGODB_NAME || "",
-
-    NOTES: process.env.MONGODB_NOTES || "",
-    COLLECTIONS: process.env.MONGODB_COLLECTIONS || "",
-    OWNER: process.env.MONGODB_OWNER || "",
-  },
-};
+    jwtSecret: process.env.JWT_SECRET || "somesecrettoken",
+  
+    DB: {
+      URI:
+        process.env.MONGODB_URI ||
+        "mongodb+srv://movilesproyecto:12345@cluster0.htp15ow.mongodb.net/test",
+   
+      USER: process.env.MONGODB_USER || "",
+  
+      PASSWORD: process.env.MONGODB_PASSWORD || "",
+  
+      NAME: process.env.MONGODB_NAME || "",
+  
+      LAST_NAME: process.env.MONGODB_NAME || "",
+  
+      NOTES: process.env.MONGODB_NOTES || "",
+      COLLECTIONS: process.env.MONGODB_COLLECTIONS || "",
+      OWNER: process.env.MONGODB_OWNER || "",
+    },
+  };
